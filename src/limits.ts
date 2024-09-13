321f43fab875b77193483607068646f555948cfd
Sergio Gil <sergio.gil@ironhack.com>
2024-09-13 09:59:13 +0100

    Changes limits

--- src/limits.ts
+++ src/limits.ts
@@ -16,8 +16,8 @@ export class TokenLimits {
       this.maxTokens = 8000
       this.responseTokens = 2000
     } else {
-      this.maxTokens = 4000
-      this.responseTokens = 1000
+      this.maxTokens = 128000
+      this.responseTokens = 28000
     }
     // provide some margin for the request tokens
     this.requestTokens = this.maxTokens - this.responseTokens - 100
